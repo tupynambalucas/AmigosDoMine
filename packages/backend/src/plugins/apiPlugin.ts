016f6a6fac2f3974a34965a3284de5ab2b6175c7
Tupynambá Lucas <tupynambalucas@gmail.com>
2025-12-31 19:44:37 -0300

    Fix: arquivos removidos

---
+++ packages/backend/src/plugins/apiPlugin.ts
@@ -0,0 +1,9 @@
+import type { FastifyPluginAsync, FastifyInstance } from 'fastify';
+import authRoutes from '../domains/auth/auth.routes.js';
+
+const apiPlugin: FastifyPluginAsync = async function (server: FastifyInstance) {
+  
+  await server.register(authRoutes, { prefix: 'auth' });
+}
+
+export default apiPlugin;
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,7 @@
+export * from './constants';
+export * from './schemas/user.schema';
+export * from './schemas/product.schema';
+export * from './schemas/cycle.schema';
+export * from './schemas/auth.schema';
+
+export * from './locales/pt-br'
