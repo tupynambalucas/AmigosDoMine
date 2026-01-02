bf73748ac0c12acb42744e6e68b1471370d71eed
Tupynambá Lucas <tupynambalucas@gmail.com>
2026-01-02 08:05:27 -0300

    refact struct

--- packages/resources-devlopment/masterplugin/build.gradle.kts
+++ packages/resources-devlopment/masterplugin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.0"
-    id("com.gradleup.shadow") version "8.3.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
@@ -19,14 +19,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
-tasks {
-    runServer {
-        // Configure the Minecraft version for our task.
-        // This is the only required configuration besides applying the plugin.
-        // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21")
-    }
-}
 
 val targetJavaVersion = 21
 kotlin {
